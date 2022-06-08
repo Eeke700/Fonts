bc21a9b794e9f6ac979ded8f37012edba80cf26d
Eeke700 <74800670+Eeke700@users.noreply.github.com>
2022-06-08 15:14:06 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import http from 'http';
 import {file} from './i.mjs';
 
 const listener = (req,res)=>{
-  try{res.end(file(req.url.split('/').slice(-2)[0]))}
+  try{res.end(file(req.url.split('/').slice(-1)[0]))}
   catch(e){res.end(e.message)}
 };
 http.createServer(listener).listen(3030);
