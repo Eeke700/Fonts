820fa31c9d5656838e78e698fb29153c3f39b714
Eeke700 <74800670+Eeke700@users.noreply.github.com>
2022-06-08 14:33:39 +0200

    Update i.mjs

--- i.mjs
+++ i.mjs
@@ -2,17 +2,14 @@ export const file =(f,fo=f.split('+').join(' '))=> `<!DOCTYPE html>
 <html id="html">
     <head>
 <style id="style">
-@import url('https://fonts.googleapis.com/css2?family=${f}&display=swap')
+@import url('https://fonts.googleapis.com/css2?family=${f}&display=swap');
 body{
 display:flex;align-items:center;justify-content:center;height:100vh;width:100vw;font-size:3em;font-family:"${fo}";
 }
-
 </style>
 </head>
 <body>
 ${fo}
 </body>
-
-</html>
-
+</html>`
 `
