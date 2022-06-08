b5744fff59009b29dea5bc8c43617d183db148b6
Eeke700 <74800670+Eeke700@users.noreply.github.com>
2022-06-08 14:06:12 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -3,4 +3,4 @@ import * as fs from 'fs/promises';
 
 const file = await fs.readFile('./i.html');
 const listener = (req,res)=>res.end(file);
-http.createServer(listener).listen(8000);
+http.createServer(listener).listen(3030);
